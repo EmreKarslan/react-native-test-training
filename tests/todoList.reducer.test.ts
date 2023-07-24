c11b3f84bfbb105c80089f0d69ea6ebfe01c4826
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2023-07-24 11:22:18 +0300

    feat(tests): Add new tests for todo reducer

--- tests/todoList.reducer.test.ts
+++ tests/todoList.reducer.test.ts
@@ -1,56 +1,106 @@
-import { initialState } from "@/store/reducers/todoList/todoList.contants";
-import {
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer, {
   addTodo,
   clearTodo,
   removeTodo,
-  todoSlice,
   toggleTodo,
 } from "@/store/reducers/todoList/todoList.reducer";
 
-describe("todoSlice addTodo", () => {
+describe("todoSlice reducer", () => {
+  let store;
+
+  beforeEach(() => {
+    store = configureStore({ reducer: todoReducer });
+  });
+
   it("should add a new todo to the state", () => {
     const mockTodo = { id: 1, text: "Test todo", isDone: false };
-    const newState = todoSlice.reducer(initialState, addTodo(mockTodo));
+    store.dispatch(addTodo(mockTodo));
 
-    expect(newState.todos).toHaveLength(1);
+    expect(store.getState().todos).toHaveLength(1);
   });
-});
 
-describe("todoSlice remove", () => {
-  it("should remove the specified todo from state", () => {
+  it("should not add a todo with a duplicated id", () => {
+    const initialState = {
+      todos: [
+        { id: 1, text: "First Todo", isDone: true },
+        { id: 2, text: "Second Todo", isDone: false },
+      ],
+    };
+    store = configureStore({
+      reducer: todoReducer,
+      preloadedState: initialState,
+    });
+
+    const duplicatedTodo = { id: 1, text: "Test todo", isDone: false };
+
+    expect(() => store.dispatch(addTodo(duplicatedTodo))).toThrowError(
+      "Cannot add todo: A todo with the same ID already exists. Please ensure each todo has a unique ID.",
+    );
+  });
+
+  it("should remove the specified todo from state (with valid Id)", () => {
     const initialStateWithTodos = {
-      ...initialState,
       todos: [{ id: 1, text: "Test 1 todo", isDone: true }],
     };
-    const newState = todoSlice.reducer(initialStateWithTodos, removeTodo(1));
+    store = configureStore({
+      reducer: todoReducer,
+      preloadedState: initialStateWithTodos,
+    });
 
-    expect(newState.todos).toHaveLength(1);
+    store.dispatch(removeTodo(1));
+
+    expect(store.getState().todos).toHaveLength(0);
   });
-});
 
-describe("todoSlice toggleTodo", () => {
+  it("should remove the specified todo from state (with invalid Id)", () => {
+    const initialStateWithTodos = {
+      todos: [{ id: 1, text: "Test 1 todo", isDone: true }],
+    };
+    store = configureStore({
+      reducer: todoReducer,
+      preloadedState: initialStateWithTodos,
+    });
+
+    store.dispatch(removeTodo(2));
+
+    expect(store.getState().todos).toHaveLength(1);
+  });
+
+  it("should remove the specified todo from state (without todos)", () => {
+    store.dispatch(removeTodo(1));
+
+    expect(store.getState().todos).toHaveLength(0);
+  });
+
   it("should toggle the 'isDone' property of the specified todo", () => {
     const initialStateWithTodo = {
-      ...initialState,
       todos: [{ id: 1, text: "Test todo", isDone: false }],
     };
-    const newState = todoSlice.reducer(initialStateWithTodo, toggleTodo(0));
+    store = configureStore({
+      reducer: todoReducer,
+      preloadedState: initialStateWithTodo,
+    });
 
-    expect(newState.todos[0].isDone).toBe(true); // It should toggle from false to true
+    store.dispatch(toggleTodo(0));
+
+    expect(store.getState().todos[0].isDone).toBe(true); // It should toggle from false to true
   });
-});
 
-describe("todoSlice clearTodo", () => {
   it("should clear all todos from the state", () => {
     const initialStateWithTodos = {
-      ...initialState,
       todos: [
         { id: 1, text: "Test todo 1", isDone: false },
         { id: 2, text: "Test todo 2", isDone: true },
       ],
     };
-    const newState = todoSlice.reducer(initialStateWithTodos, clearTodo());
+    store = configureStore({
+      reducer: todoReducer,
+      preloadedState: initialStateWithTodos,
+    });
 
-    expect(newState.todos).toHaveLength(0);
+    store.dispatch(clearTodo());
+
+    expect(store.getState().todos).toHaveLength(0);
   });
 });
