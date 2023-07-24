aef61a73a9f747b91a17c9b6b2574827318f12c9
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2023-07-24 05:41:13 +0300

    test: add test cases for counterReducer

--- src/store/reducers/counter/counter.reducer.ts
+++ src/store/reducers/counter/counter.reducer.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { initialState } from "./counter.constants";
 
-const counterSlice = createSlice({
+export const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
