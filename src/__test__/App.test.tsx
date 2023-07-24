b22ee444d6f9cbd5a67ded08b88a1cf647981dd1
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2023-07-24 05:52:36 +0300

    chore: test file relocated

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import { countChildren } from "@/utils/test/test.utils";
 import renderer from "react-test-renderer";
-
-import App from "./App";
-import { countChildren } from "./utils/test/test.utils";
+import React from "react";
+import App from "@/App";
 
 describe("<App />", () => {
   it("has 1 child", () => {
