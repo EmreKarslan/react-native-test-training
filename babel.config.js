6c795db5e90a2c183ac31e458871a245e1b5dd4b
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2023-07-24 04:59:36 +0300

    feat: absolute path defination added

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ".",
+          alias: {
+            "@/components": "./src/components",
+            "@/util": "./src/util",
+            "@/screens": "./src/screens",
+            "@/navigation": "./src/navigation",
+            "@/constants": "./src/constants",
+            "@/assets": "./src/assets",
+            "@/hooks": "./src/hooks",
+            "@/icons": "./src/icons",
+            "@/store": "./src/store",
+            "@/types": "./src/types",
+          },
+        },
+      ],
+    ],
   };
 };
